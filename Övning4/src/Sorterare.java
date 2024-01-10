05e8f333c0022fb42743ba7b47062dcbe96ae474
Fluntin <112729558+Fluntin@users.noreply.github.com>
2024-01-10 22:29:06 +0100

    God willing it is done.

--- Övning4/src/Sorterare.java
+++ Övning4/src/Sorterare.java
@@ -1,54 +1,60 @@
 import java.util.Arrays;
 import java.util.HashMap;
-import java.util.Set;
-
 
 public class Sorterare implements Runnable {
-	Filläsare läsare;
-	HP ägare;
-	String nyttOrd;
-	HashMap<String,Integer> räknare;
-	
-	public Sorterare(Filläsare läsare, HP ägare){
-		räknare=new HashMap<String, Integer>();
-		this.läsare=läsare;
-		this.ägare=ägare;
-		new Thread(this, "Sorterare").start();
-	}
-	@Override
-	public void run() {
-		// TODO Auto-generated method stub
-			update();
-			while(!"".equals(nyttOrd)){
-				if(räknare.containsKey(nyttOrd)){
-					räknare.put(nyttOrd, new Integer(räknare.get(nyttOrd)+1));
-				}
-				else{
-					räknare.put(nyttOrd, new Integer(1));
-				}
-				synchronized(läsare){
-					läsare.notify();
-				}
-				update();
-			}
-			String[] ord=räknare.keySet().toArray(new String[0]);
-			Arrays.sort(ord);
-			ägare.skrivUt(ord, räknare);
-			synchronized(läsare){
-				läsare.notify();
-			}
-	}
-	
-	public void update(){
-		nyttOrd=null;
+    Filläsare läsare;
+    HP ägare;
+    String nyttOrd;
+    HashMap<String, Integer> räknare;
+
+    public Sorterare(Filläsare läsare, HP ägare) {
+        räknare = new HashMap<>();
+        this.läsare = läsare;
+        this.ägare = ägare;
+        new Thread(this, "Sorterare").start();
+    }
+
+    @Override
+    public void run() {
+        update();
+        while (!"".equals(nyttOrd)) {
+            // Synchronize access to the räknare HashMap
+            synchronized (räknare) {
+                räknare.put(nyttOrd, räknare.getOrDefault(nyttOrd, 0) + 1);
+            }
+
+            // Notify the läsare (reader) thread to continue reading
+            synchronized (läsare) {
+                läsare.notify();
+            }
+
+            update();
+        }
+
+        String[] ord;
+        // Synchronize access to the räknare HashMap while getting the keys
+        synchronized (räknare) {
+            ord = räknare.keySet().toArray(new String[0]);
+        }
+        Arrays.sort(ord);
+        ägare.skrivUt(ord, räknare);
+
+        // Notify the läsare (reader) thread to continue reading
+        synchronized (läsare) {
+            läsare.notify();
+        }
+    }
+
+    public void update() {
+        nyttOrd = null;
         try {
-    		synchronized(this){
-    			wait();
-    		}
-        } 
-        catch (InterruptedException e) {
+            synchronized (this) {
+                wait();
+            }
+        } catch (InterruptedException e) {
             e.printStackTrace();
         }
-        nyttOrd=läsare.aktuellText;
-	}
+        nyttOrd = läsare.aktuellText;
+    }
 }
+
